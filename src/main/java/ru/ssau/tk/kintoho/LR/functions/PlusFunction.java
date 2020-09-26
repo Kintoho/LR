c0ea4fffa676f28151b7bc5bbdd85f0d2f064fdd
Kintoho <hohlov.aleksandr.a@gmail.com>
2020-09-26 09:29:26 +0400

    Создан класс PlusFunction

---
+++ src/main/java/ru/ssau/tk/kintoho/LR/functions/PlusFunction.java
@@ -0,0 +1,7 @@
+package ru.ssau.tk.kintoho.LR.functions;
+
+public class PlusFunction implements MathFunction{
+        public double apply(double x) {
+            return x+5;
+        }
+}
---
+++ src/test/java/ru/ssau/tk/kintoho/LR/functions/PlusFunctionTest.java
@@ -0,0 +1,12 @@
+package ru.ssau.tk.kintoho.LR.functions;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class PlusFunctionTest {
+
+    @Test
+    public void testApply() {
+    }
+}
