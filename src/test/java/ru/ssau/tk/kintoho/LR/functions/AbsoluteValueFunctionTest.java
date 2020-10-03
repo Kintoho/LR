f3550b999cb24af003b704a00e594a195e7f6277
Kintoho <hohlov.aleksandr.a@gmail.com>
2020-10-03 09:25:56 +0400

    Создан тест SqrFunctionTest

--- src/test/java/ru/ssau/tk/kintoho/LR/functions/AbsoluteValueFunctionTest.java
+++ src/test/java/ru/ssau/tk/kintoho/LR/functions/AbsoluteValueFunctionTest.java
@@ -12,7 +12,7 @@ public class AbsoluteValueFunctionTest {
         MathFunction testFunction = new AbsoluteValueFunction();
         assertEquals(testFunction.apply(1.0), 1.0, DELTA);
         assertNotEquals(testFunction.apply(2.0), 3.0, DELTA);
-        assertEquals(testFunction.apply(123.0), 123.0, DELTA);
+        assertEquals(testFunction.apply(-123.0), 123.0, DELTA);
         assertEquals(testFunction.apply(666.0), 666.0, DELTA);
         assertNotEquals(testFunction.apply(2.0), -2.0, DELTA);
     }
--- src/test/java/ru/ssau/tk/kintoho/LR/functions/SqrFunctionTest.java
+++ src/test/java/ru/ssau/tk/kintoho/LR/functions/SqrFunctionTest.java
@@ -5,9 +5,13 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
 public class SqrFunctionTest {
-
+    private final static double DELTA = 0.00001;
     @Test
     public void testApply() {
-
+        IdentityFunction testFunction = new IdentityFunction();
+        assertEquals(testFunction.apply(4), 2.0, DELTA);
+        assertNotEquals(testFunction.apply(2.0), 2.0, DELTA);
+        assertEquals(testFunction.apply(64.0), 8.0, DELTA);
+        assertEquals(testFunction.apply(36), 36, DELTA);
     }
 }
