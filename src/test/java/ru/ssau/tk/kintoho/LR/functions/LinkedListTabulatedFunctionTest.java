310c8864ac26dd9d762c2fe08a4d078adba624cb
Golovushkin <golovushkin_aa@mail.ru>
2020-10-23 23:09:08 +0400

    Начал делать LinkedListTabulatedFunctionTest

---
+++ src/test/java/ru/ssau/tk/kintoho/LR/functions/LinkedListTabulatedFunctionTest.java
@@ -0,0 +1,12 @@
+package ru.ssau.tk.kintoho.LR.functions;
+
+import static org.testng.Assert.*;
+
+import org.testng.annotations.Test;
+
+public class LinkedListTabulatedFunctionTest {
+    private final static double DELTA = 0.001;
+    private final double[] valuesX = new double[]{-6., -3., -1., 0., 1., 3., 6.};
+    private final double[] valuesY = new double[]{-12., -6., -3., 0., 3., 6., 12.};
+
+}
