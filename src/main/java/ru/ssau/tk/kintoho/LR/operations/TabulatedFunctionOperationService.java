31faeecf6a3fa6bea26d90e5d6208d6ba742ba1b
Kintoho <hohlov.aleksandr.a@gmail.com>
2020-12-11 21:03:39 +0400

    Созданы InconsistentFunctionsException and TabulatedFunctionOperationService

--- src/main/java/ru/ssau/tk/kintoho/LR/operations/TabulatedFunctionOperationService.java
+++ src/main/java/ru/ssau/tk/kintoho/LR/operations/TabulatedFunctionOperationService.java
@@ -1,8 +1,28 @@
 package ru.ssau.tk.kintoho.LR.operations;
 
 import ru.ssau.tk.kintoho.LR.functions.*;
+import ru.ssau.tk.kintoho.LR.functions.factory.ArrayTabulatedFunctionFactory;
+import ru.ssau.tk.kintoho.LR.functions.factory.TabulatedFunctionFactory;
 
 public class TabulatedFunctionOperationService {
+    private TabulatedFunctionFactory factory;
+
+    public TabulatedFunctionOperationService(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
+
+    public TabulatedFunctionOperationService() {
+        this.factory = new ArrayTabulatedFunctionFactory();
+    }
+
+    public TabulatedFunctionFactory getFactory() {
+        return factory;
+    }
+
+    public void setFactory(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
+
     public static Point[] asPoints(TabulatedFunction tabulatedFunction) {
         int i = 0;
         Point[] points = new Point[tabulatedFunction.getCount()];
@@ -11,4 +31,6 @@ public class TabulatedFunctionOperationService {
         }
         return points;
     }
+
+
 }
