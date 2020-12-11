f1f0ce8bdd2c5709504686c637929a817f4dd2a2
Kintoho <hohlov.aleksandr.a@gmail.com>
2020-12-11 23:10:51 +0400

    Реализация метода doOperation

--- src/main/java/ru/ssau/tk/kintoho/LR/operations/TabulatedFunctionOperationService.java
+++ src/main/java/ru/ssau/tk/kintoho/LR/operations/TabulatedFunctionOperationService.java
@@ -1,6 +1,8 @@
 package ru.ssau.tk.kintoho.LR.operations;
 
-import ru.ssau.tk.kintoho.LR.functions.*;
+import ru.ssau.tk.kintoho.LR.exceptions.InconsistentFunctionsException;
+import ru.ssau.tk.kintoho.LR.functions.Point;
+import ru.ssau.tk.kintoho.LR.functions.TabulatedFunction;
 import ru.ssau.tk.kintoho.LR.functions.factory.ArrayTabulatedFunctionFactory;
 import ru.ssau.tk.kintoho.LR.functions.factory.TabulatedFunctionFactory;
 
@@ -32,5 +34,42 @@ public class TabulatedFunctionOperationService {
         return points;
     }
 
+    private interface BiOperation {
+        double apply(double u, double v);
+    }
+
+    private TabulatedFunction doOperation(TabulatedFunction a, TabulatedFunction b, BiOperation operation){
+        if (a.getCount()!=b.getCount()){
+            throw new InconsistentFunctionsException();
+        }
+        Point[] aPoints = asPoints(a);
+        Point[] bPoints = asPoints(b);
+        double[] xValues = new double[a.getCount()];
+        double[] yValues = new double[b.getCount()];
+        for (int i = 0; i < xValues.length; i++) {
+            if (aPoints[i].x - bPoints[i].x != 0) {
+                throw new InconsistentFunctionsException();
+            }
+            xValues[i] = aPoints[i].x;
+            yValues[i] = operation.apply(aPoints[i].y, bPoints[i].y);
+        }
+        return factory.create(xValues, yValues);
+    }
+
+    public TabulatedFunction sum (TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, ((u, v) -> u + v));
+    }
+
+    public TabulatedFunction subtraction (TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, ((u, v) -> u - v));
+    }
+
+    public TabulatedFunction multiplication (TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, ((u, v) -> u * v));
+    }
+
+    public TabulatedFunction division (TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, ((u, v) -> u / v));
+    }
 
 }
