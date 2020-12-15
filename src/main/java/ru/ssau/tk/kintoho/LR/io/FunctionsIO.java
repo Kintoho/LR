a84ef4f8650ef3f5feb479227f911af8523d854a
Golovushkin <golovushkin_aa@mail.ru>
2020-12-15 19:18:27 +0400

    метод writeTabulatedFunction() и класс TabulatedFunctionFileOutputStream

--- src/main/java/ru/ssau/tk/kintoho/LR/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/kintoho/LR/io/FunctionsIO.java
@@ -1,7 +1,22 @@
 package ru.ssau.tk.kintoho.LR.io;
 
+import java.io.*;
+
+import ru.ssau.tk.kintoho.LR.functions.*;
+
 public final class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException();
     }
+
+    public static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException {
+        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+        dataOutputStream.writeInt(function.getCount());
+        for (Point point : function) {
+            dataOutputStream.writeDouble(point.x);
+            dataOutputStream.writeDouble(point.y);
+        }
+        dataOutputStream.flush();
+    }
+
 }
---
+++ src/main/java/ru/ssau/tk/kintoho/LR/io/TabulatedFunctionFileOutputStream.java
@@ -0,0 +1,17 @@
+package ru.ssau.tk.kintoho.LR.io;
+import java.io.*;
+import ru.ssau.tk.kintoho.LR.functions.*;
+
+public class TabulatedFunctionFileOutputStream {
+    public static void main(String[] args) {
+        ArrayTabulatedFunction functionArray = new ArrayTabulatedFunction(new double[]{1., 2., 3., 4,}, new double[]{5., 6., 7., 8.});
+        LinkedListTabulatedFunction functionList = new LinkedListTabulatedFunction(new double[]{1., 2., 3., 4,}, new double[]{1., 4., 6., 8.});
+        try (BufferedOutputStream outputStreamArray = new BufferedOutputStream(new FileOutputStream("output/array functionArray.bin"));
+             BufferedOutputStream outputStreamList = new BufferedOutputStream(new FileOutputStream("output/array functionList.bin"))) {
+            FunctionsIO.writeTabulatedFunction(outputStreamArray, functionArray);
+            FunctionsIO.writeTabulatedFunction(outputStreamList, functionList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
