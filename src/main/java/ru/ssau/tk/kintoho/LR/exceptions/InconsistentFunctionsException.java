14fb8272a290c58bbe4c25d651f21f4b11e12d17
Kintoho <hohlov.aleksandr.a@gmail.com>
2020-12-18 20:43:02 +0400

    Сериализация исключений

--- src/main/java/ru/ssau/tk/kintoho/LR/exceptions/InconsistentFunctionsException.java
+++ src/main/java/ru/ssau/tk/kintoho/LR/exceptions/InconsistentFunctionsException.java
@@ -1,6 +1,8 @@
 package ru.ssau.tk.kintoho.LR.exceptions;
 
 public class InconsistentFunctionsException extends RuntimeException {
+    private static final long serialVersionUID = 1269184723135709239L;
+
     public InconsistentFunctionsException() {
         super();
     }
