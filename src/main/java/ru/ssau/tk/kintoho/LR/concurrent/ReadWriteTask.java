9d1211ecc9186aab4c9dc360f5faa39cc70589aa
Golovushkin <golovushkin_aa@mail.ru>
2020-12-22 00:13:19 +0400

    1 задание

---
+++ src/main/java/ru/ssau/tk/kintoho/LR/concurrent/ReadWriteTask.java
@@ -0,0 +1,29 @@
+package ru.ssau.tk.kintoho.LR.concurrent;
+
+import ru.ssau.tk.kintoho.LR.functions.TabulatedFunction;
+
+public class ReadWriteTask implements Runnable {
+    private final TabulatedFunction tabulatedFunction;
+
+    public ReadWriteTask(TabulatedFunction tabulatedFunction) {
+        this.tabulatedFunction = tabulatedFunction;
+    }
+
+    @Override
+    public void run() {
+
+        for (int i = 0; i < tabulatedFunction.getCount(); i++) {
+            double x;
+            double y;
+            x = tabulatedFunction.getX(i);
+            synchronized (Thread.currentThread()) {
+                y = tabulatedFunction.getY(i);
+                System.out.println(Thread.currentThread().getName() + " before write: i=" + i + " " + "x=" + x + " " + "y=" + y);
+                tabulatedFunction.setY(i, y + 1);
+                y = tabulatedFunction.getY(i);
+            }
+            System.out.println(Thread.currentThread().getName() + " after write: i=" + i + " " + "x=" + x + " " + "y=" + y);
+            System.out.println();
+        }
+    }
+}
---
+++ src/main/java/ru/ssau/tk/kintoho/LR/concurrent/ReadWriteTaskExecutor.java
@@ -0,0 +1,22 @@
+package ru.ssau.tk.kintoho.LR.concurrent;
+
+import ru.ssau.tk.kintoho.LR.functions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadWriteTaskExecutor {
+    public static void main(String[] args) throws InterruptedException{
+        TabulatedFunction listFunction = new LinkedListTabulatedFunction(new ZeroFunction(), 1, 10, 10);
+        List<Thread> list = new ArrayList<>();
+        for (int i = 0; i <= 20; i++) {
+            Thread thread = new Thread(new ReadWriteTask(listFunction));
+            list.add(thread);
+        }
+        for (Thread thread : list) {
+            thread.start();
+        }
+        Thread.sleep(2000);
+    }
+
+}
