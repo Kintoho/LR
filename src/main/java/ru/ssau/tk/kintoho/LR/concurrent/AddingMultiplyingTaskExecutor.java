daa0d14b5868fc66c9689658656a120d29a9403c
Kintoho <hohlov.aleksandr.a@gmail.com>
2020-12-24 00:36:35 +0400

    Исправил мелкие ошибки

--- src/main/java/ru/ssau/tk/kintoho/LR/concurrent/AddingMultiplyingTaskExecutor.java
+++ src/main/java/ru/ssau/tk/kintoho/LR/concurrent/AddingMultiplyingTaskExecutor.java
@@ -4,13 +4,9 @@ import ru.ssau.tk.kintoho.LR.functions.ConstantFunction;
 import ru.ssau.tk.kintoho.LR.functions.LinkedListTabulatedFunction;
 import ru.ssau.tk.kintoho.LR.functions.TabulatedFunction;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class AddingMultiplyingTaskExecutor {
     public static void main(String[] args) throws InterruptedException {
         TabulatedFunction listFunction = new LinkedListTabulatedFunction(new ConstantFunction(2), 1, 100, 100);
-        List<Thread> list = new ArrayList<>();
         Thread thread0 = new Thread(new MultiplyingTask(listFunction));
         Thread thread1 = new Thread(new MultiplyingTask(listFunction));
         Thread thread2 = new Thread(new AddingTask(listFunction));
@@ -18,6 +14,8 @@ public class AddingMultiplyingTaskExecutor {
         thread1.start();
         thread2.start();
         Thread.sleep(2000);
+        System.out.println(listFunction);
+
     }
 
 }
