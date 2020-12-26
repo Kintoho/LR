fc9e7baf53b3e2943fe1671d7202236a40ed295a
Kintoho <hohlov.aleksandr.a@gmail.com>
2020-12-26 21:12:32 +0400

    Всё изменил, и думаю, что не в последний раз

--- src/main/java/ru/ssau/tk/kintoho/LR/ui/Window.java
+++ src/main/java/ru/ssau/tk/kintoho/LR/ui/Window.java
@@ -2,16 +2,28 @@ package ru.ssau.tk.kintoho.LR.ui;
 
 import javax.swing.*;
 
-public class Window {
+public class Window extends JFrame {
+    JMenu menu1, menu2;
+    JMenuItem a1, a2;
     Window(){
-        JFrame jFrame = new JFrame("Calculator");
-        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        jFrame.setSize(920, 410);
-        jFrame.setVisible(true);
-        jFrame.setLocationRelativeTo(null);
+        JFrame window = new JFrame("Calculator");
+        menu1 = new JMenu("Functions");
+        JMenuBar bar = new JMenuBar();
+        a1 = new JMenuItem("TabulatedFunction");
+        a2 = new JMenuItem("MathFunction");
+        menu1.add(a1);
+        menu1.add(a2);
+        bar.add(menu1);
+        window.setJMenuBar(bar);
+        window.setSize(900,600);
+        window.setLayout(null);
+        window.setVisible(true);
+        window.setLocationRelativeTo(null);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         new Window();
     }
 }
