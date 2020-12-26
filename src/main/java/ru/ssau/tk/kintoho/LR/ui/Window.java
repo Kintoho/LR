19043e838223a88c09baaa05f92a0fcd174c84f9
Kintoho <hohlov.aleksandr.a@gmail.com>
2020-12-26 20:29:35 +0400

    Беды с меню

--- src/main/java/ru/ssau/tk/kintoho/LR/ui/Window.java
+++ src/main/java/ru/ssau/tk/kintoho/LR/ui/Window.java
@@ -3,10 +3,15 @@ package ru.ssau.tk.kintoho.LR.ui;
 import javax.swing.*;
 
 public class Window {
-    public static void main(String[] args){
+    Window(){
         JFrame jFrame = new JFrame("Calculator");
         jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        jFrame.setBounds(340, 120, 920, 410);
+        jFrame.setSize(920, 410);
         jFrame.setVisible(true);
+        jFrame.setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args){
+        new Window();
     }
 }
