6d8b4c4c2a0eada4332e7e8dc0f77c0a156119ba
Kintoho <hohlov.aleksandr.a@gmail.com>
2020-12-26 17:22:15 +0400

    Создал окно, в котором мы будем творить (грязь)

---
+++ src/main/java/ru/ssau/tk/kintoho/LR/ui/Window.java
@@ -0,0 +1,12 @@
+package ru.ssau.tk.kintoho.LR.ui;
+
+import javax.swing.*;
+
+public class Window {
+    public static void main(String[] args){
+        JFrame jFrame = new JFrame("Calculator");
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jFrame.setBounds(340, 120, 920, 410);
+        jFrame.setVisible(true);
+    }
+}
