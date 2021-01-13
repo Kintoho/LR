52958001272decae4615fa44a30d2629bd1c50b5
Golovushkin <golovushkin_aa@mail.ru>
2021-01-13 23:09:39 +0400

    3.2 X*

--- src/main/java/ru/ssau/tk/kintoho/LR/functions/factory/TabulatedFunctionFactory.java
+++ src/main/java/ru/ssau/tk/kintoho/LR/functions/factory/TabulatedFunctionFactory.java
@@ -1,9 +1,15 @@
 package ru.ssau.tk.kintoho.LR.functions.factory;
 
+import ru.ssau.tk.kintoho.LR.functions.StrictTabulatedFunction;
 import ru.ssau.tk.kintoho.LR.functions.MathFunction;
 import ru.ssau.tk.kintoho.LR.functions.TabulatedFunction;
 
 public interface TabulatedFunctionFactory {
     TabulatedFunction create(double[] xValues, double[] yValues);
+
     TabulatedFunction create(MathFunction source, double xFrom, double xTo, int count);
+
+    default TabulatedFunction createStrict(double[] xValues, double[] yValues) {
+        return new StrictTabulatedFunction(create(xValues, yValues));
+    }
 }
--- src/test/java/ru/ssau/tk/kintoho/LR/functions/factory/TabulatedFunctionFactoryTest.java
+++ src/test/java/ru/ssau/tk/kintoho/LR/functions/factory/TabulatedFunctionFactoryTest.java
@@ -6,16 +6,25 @@ import ru.ssau.tk.kintoho.LR.functions.*;
 import static org.testng.Assert.assertTrue;
 
 public class TabulatedFunctionFactoryTest {
+    double[] xValues = new double[]{1., 2., 3., 4., 5.};
+    double[] yValues = new double[]{6., 7., 8., 9., 10.};
+    private final TabulatedFunctionFactory array = new ArrayTabulatedFunctionFactory();
+    private final TabulatedFunctionFactory list = new LinkedListTabulatedFunctionFactory();
 
     @Test
     public void testCreate() {
-        double[] xValues = new double[]{1., 2., 3., 4., 5.};
-        double[] yValues = new double[]{6., 7., 8., 9., 10.};
-
         LinkedListTabulatedFunctionFactory testFirstFunction = new LinkedListTabulatedFunctionFactory();
         ArrayTabulatedFunctionFactory testSecondFunction = new ArrayTabulatedFunctionFactory();
 
         assertTrue(testFirstFunction.create(xValues, yValues) instanceof LinkedListTabulatedFunction);
         assertTrue(testSecondFunction.create(xValues, yValues) instanceof ArrayTabulatedFunction);
+    }
+
+    @Test
+    public void testCreateStrict() {
+        TabulatedFunction function = array.createStrict(xValues, yValues);
+        assertTrue(function instanceof StrictTabulatedFunction);
+        TabulatedFunction function1 = list.createStrict(xValues, yValues);
+        assertTrue(function1 instanceof StrictTabulatedFunction);
     }
 }
